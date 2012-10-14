30587b20977dce6328eabea3b527c0fe51b9ab2e
Frank Staals <frank@Hina.local>
2012-10-14 11:14:07 +0200

    more fixes in the wrappers

--- src/main/scala/net/fstaals/model/ActivityFile.scala
+++ src/main/scala/net/fstaals/model/ActivityFile.scala
@@ -1,7 +1,7 @@
 package net.fstaals.model
 
 import de.saring.exerciseviewer.parser.ExerciseParser
-import de.saring.exerciseviewer.data.EVExercise
+import de.saring.exerciseviewer.data._
 import de.saring.exerciseviewer.parser.ExerciseParserFactory
 
 import org.joda.time.DateTime
@@ -33,9 +33,9 @@ case class ActivityFile(val path : String) {
     trajectory   = Trajectory.fromEVSamples(rawData.getSampleList().toList)
   }
 
-  def start : Option[DateTime]  = activityData map {ev => new DateTime(ev.getDate())}
+  def start : Option[DateTime]    = activityData map {e => new DateTime(e.getDate())}
 
-  def end   : Option[DateTime]  = start map {
+  def end   : Option[DateTime]    = start map {
     // this trajectory should not be empty
     val dur = Duration.millis(trajectory.endPoint.timestamp)
     _ + dur
@@ -43,18 +43,49 @@ case class ActivityFile(val path : String) {
 
 //  def heartRate() = activityData map {_
 
-  def avgSpeed : Option[Double] = activityData map {_.getSpeed().getSpeedAVG()}
+  def speed : Option[Speed]       = activityData map {e =>
+    Speed.fromESpeed(e.getSpeed())}
 
-  def maxSpeed : Option[Double] = activityData map {_.getSpeed().getSpeedMax()}
+  def distance : Option[Int]      = activityData map {_.getSpeed().getDistance()}
 
-  def distance : Option[Int]    = activityData map {_.getSpeed().getDistance()}
+  def altitude : Option[Altitude] =
+    activityData map {e => Altitude.fromEAltitude(e.getAltitude())}
 
-  def altitude : Option[Int]    = activityData map {_.getAltitude()}
+  def cadence : Option[Cadence]   =
+    activityData map {e => Cadence.fromECadence(e.getCadence())}
 
-  def cadence : Option[Short]   = activityData map {_.getCadence()}
-
-  def temperature() = activityData map {_.getTemperature()}
+  def temperature : Option[Temperature] =
+    activityData map {e => Temperature.fromETemperature(e.getTemperature())}
 
 //  def laps() = activityData map {_.getLapList().toList()}
 
+}
+
+case class Speed(val avg: Double, val max: Double)
+
+object Speed {
+  def fromESpeed(e : ExerciseSpeed) = Speed(e.getSpeedAVG(), e.getSpeedMax())
+}
+
+case class Altitude(val avg: Int, val min: Int, val max: Int, val ascent : Int)
+
+object Altitude {
+  def fromEAltitude(e: ExerciseAltitude) =
+    Altitude(e.getAltitudeAVG(),e.getAltitudeMin(), e.getAltitudeMax(), e.getAscent())
+}
+
+case class HeartRate(val avg: Short, val max: Short)
+
+case class Cadence(val avg: Short, val max: Short)
+
+object Cadence {
+  def fromECadence(e : ExerciseCadence) = Cadence(e.getCadenceAVG(), e.getCadenceMax())
+}
+
+
+case class Temperature(val avg: Short, val min: Short, val max: Short)
+
+object Temperature {
+  def fromETemperature(e : ExerciseTemperature) =
+    Temperature(e.getTemperatureAVG(),e.getTemperatureMin(),e.getTemperatureMax())
 }
