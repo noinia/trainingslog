1796cc7edeeffa77aa2be1b36a57ad21df8607b1
Frank Staals <frank@Hina.local>
2013-02-10 13:07:11 +0100

    fixed the issue with the javascript stuff not being included correctly. Apparently you cannot youse <script src="..."/> but you *HAVE* to use <script ...></script>

--- src/main/webapp/js/graphs.js
+++ src/main/webapp/js/graphs.js
@@ -1,8 +1,9 @@
-
-function flotselection() {
-    $("#graphArea").bind( "plotselected", function( event, ranges ) {
-        alert("You selected " + ranges.xaxis.from + " to " + ranges.xaxis.to)
-        // similar for yaxis - with multiple axes, the extra ones are in
-        // x2axis, x3axis, ...
+function flotSelection(f) {
+    $(document).ready(function() {
+        $("#graphArea").bind( "plotselected", function( event, ranges ) {
+            f(ranges.xaxis.from,ranges.xaxis.to)
+            // similar for yaxis - with multiple axes, the extra ones are in
+            // x2axis, x3axis, ...
+        });
     });
 }
