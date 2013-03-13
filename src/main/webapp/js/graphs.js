25b68357ac4cee2ae38c727ce1319730dffbbba5
Frank Staals <frank@Hina.local>
2013-03-13 21:11:43 +0100

    Display values in the legend when hovering the mouse over the graph

--- src/main/webapp/js/graphs.js
+++ src/main/webapp/js/graphs.js
@@ -2,13 +2,13 @@ function onHover(plot) {
     $(document).ready( function() {
     $("#graphArea").bind("plothover", function(event, position, item) {
 
-
-        var t = Math.max(0,position.x)
+        var axes = plot.getAxes()
+        var t = Math.min(Math.max(axes.xaxis.min, position.x), axes.xaxis.max)
 
         $(".time").text(showDate(new Date(Math.floor(t))))
         showFunction("Heart Rate",  "#graphIndicator .heartRate",   ".hr",   t)
         showFunction("Power",       "#graphIndicator .power",       ".pwr",  t)
-        showFunction("Elevation",   "#graphIndicator .elevation",   ".elev", t)
+        showFunction("Altitude",    "#graphIndicator .elevation",   ".elev", t)
         showFunction("Temperature", "#graphIndicator .temperature", ".temp", t)
     })
 
@@ -23,10 +23,34 @@ function onHover(plot) {
 
     // return the function value of the function with label fLabel at time t
     function getValue(fLabel, t) {
-        // var funcs = plot.getData()
-        // alert(plot == undefined)
-
+        var series = plot.getData();
+        for (var i = 0; i < series.length; ++i)
+            if (series[i].label == fLabel) {
+                return getValueFromSeries(series[i],t)
+            }
         return null
+    }
+
+    function getValueFromSeries(series, t) {
+        function linear(p,q,alpha) { return (1 - alpha) * p + alpha * q }
+        function inBetween(p,q) {
+            return p == q ? p : linear(p[1],q[1], (t - p[0]) / (q[0] -p[0]))
+        }
+
+        var i = 0
+
+        while( i < series.data.length && series.data[i][0] <= t){
+            i++;
+        }
+
+        // alert("i: "+ i + "[i-1]: "+ series.data[i-1] + " [i]: " + series.data[i])
+
+        var p = series.data[i-1]
+        var q = series.data[Math.min(i,series.data.length-1)]
+
+        // alert("p: "+p+" q: "+q)
+
+        return inBetween(p,q).toFixed(1)
     }
 
     function showDate(d) {
