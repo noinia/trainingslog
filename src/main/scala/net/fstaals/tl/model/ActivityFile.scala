2c73815cabcb1a2f056d829ac886815801e74a19
Frank Staals <frank@Hina.local>
2013-03-23 15:53:10 +0100

    implemented stuff related to laps

--- src/main/scala/net/fstaals/tl/model/ActivityFile.scala
+++ src/main/scala/net/fstaals/tl/model/ActivityFile.scala
@@ -74,7 +74,21 @@ case class ActivityFile(val path : String) extends HasSummaryData {
 
   def trajectory : Option[TrajectoryLike] = Some(trajec)
 
-//  def laps() = activityData map {_.getLapList().toList()}
+  lazy val laps : List[Lap] =
+    activityData.toList flatMap {a => mkLaps(a.getLapList().toList)}
+
+  def mkLaps(ls: List[de.saring.exerciseviewer.data.Lap]) = {
+    def mkStart(t: Int) : Option[Duration] = Some(new Duration(t * 100))
+
+    val timestamps = 0 :: (ls map {_.getTimeSplit()})
+    val times      = (timestamps map mkStart) ++ List(duration)
+    val startEnds  = times zip times.tail
+
+    (ls zip startEnds).zipWithIndex map {case ((l,(s,e)),i) =>
+      Lap.fromELap(Some(i.shortValue()),s,e,l,trajectory)}
+  }
+
+
 
   def heartRate : Option[HeartRateSummary] =
     activityData map {e => {
@@ -92,6 +106,9 @@ case class SpeedSummary(val avg: Speed, val max: Speed)
 object SpeedSummary {
   def fromESpeed(e : ExerciseSpeed) =
     SpeedSummary(e.getSpeedAVG(), e.getSpeedMax())
+
+  def fromLSpeed(l: LapSpeed) =
+    SpeedSummary(l.getSpeedAVG(),math.max(l.getSpeedAVG(),l.getSpeedEnd()))
 }
 
 case class AltitudeSummary(val avg: Altitude, val min: Altitude, val max: Altitude, val gain : Altitude)
@@ -101,6 +118,11 @@ object AltitudeSummary {
     AltitudeSummary(e.getAltitudeAVG(),
                      e.getAltitudeMin(), e.getAltitudeMax(),
                      e.getAscent())
+
+  def fromLAltitude(l: LapAltitude) = {
+    val e = l.getAltitude() ; val a = l.getAscent()
+    AltitudeSummary(e,e,e,a)
+  }
 }
 
 case class HeartRateSummary(val avg: HeartRate, val max: HeartRate)
@@ -110,6 +132,8 @@ case class CadenceSummary(val avg: Cadence, val max: Cadence)
 object CadenceSummary {
   def fromECadence(e : ExerciseCadence) =
     CadenceSummary(e.getCadenceAVG(), e.getCadenceMax())
+
+  def fromLSpeed(l: LapSpeed) = { val c = l.getCadence() ; CadenceSummary(c,c) }
 }
 
 
@@ -123,4 +147,59 @@ object TemperatureSummary {
   def fromETemperature(e : ExerciseTemperature) =
     TemperatureSummary(e.getTemperatureAVG(),
                         e.getTemperatureMin(),e.getTemperatureMax())
+
+  def fromLTemperature(l: LapTemperature) = {
+    val t = l.getTemperature()
+    TemperatureSummary(t,t,t)
+  }
+
+}
+
+
+
+
+case class Lap( val lapNumber     : Option[Short]
+              , val startTime     : Option[Duration]
+              , val endTime       : Option[Duration]
+              , val speed         : Option[SpeedSummary]
+              , val distance      : Option[Distance]
+              , val heartRate     : Option[HeartRateSummary]
+              , val altitude      : Option[AltitudeSummary]
+              , val cadence       : Option[CadenceSummary]
+              , val temperature   : Option[TemperatureSummary]
+              , val power         : Option[PowerSummary]
+              , val genTrajectory : () => Option[TrajectoryLike]
+             ) extends HasSummaryData {
+
+  def duration = None // todo
+
+  def trajectory = genTrajectory()
+
+}
+
+object Lap {
+  def fromELap(i: Option[Short], start: Option[Duration], end: Option[Duration],
+               l: de.saring.exerciseviewer.data.Lap,
+               fullTraj: Option[TrajectoryLike]) = {
+
+    val hr = (Option(l.getHeartRateAVG()),Option(l.getHeartRateMax())) match {
+      case (Some(a),Some(m)) => Some(HeartRateSummary(a,m))
+      case _                 => None
+    }
+
+    val dist = Option(l.getSpeed().getDistance())
+
+    lazy val subTrajectory = None // TODO        fullTraj.subTrajectory()
+
+    Lap(i,start,end,
+        Option(l.getSpeed()) map {SpeedSummary.fromLSpeed(_)},
+        dist,
+        hr,
+        Option(l.getAltitude()) map {AltitudeSummary.fromLAltitude(_)},
+        Option(l.getSpeed()) map {CadenceSummary.fromLSpeed(_)},
+        Option(l.getTemperature()) map {TemperatureSummary.fromLTemperature(_)},
+        None, // power
+        () => subTrajectory
+      )
+  }
 }
